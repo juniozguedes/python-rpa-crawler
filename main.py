37577ed65d47bec26a78eb9167b0f3b1d832573c
Wellington <juniozguedes@gmail.com>
2023-07-15 14:30:42 -0300

    Better naming functions

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from time import sleep
 from RPA.Browser.Selenium import Selenium
 from constants import (
     BREADCRUMB_BUTTON,
+    CATEGORY_SECTION,
     CATEGORY_SELECTION,
     NEWS_CATEGORY,
+    NEWS_SELECTION,
     NYTIMES_URL,
     SEARCH_BUTTON,
     SEARCH_PHRASE,
@@ -21,15 +23,29 @@ def click(locator):
     browser_lib.click_element_when_visible(locator)
 
 
-def type_search(locator):
+def type_search(search_phrase):
     search_input = "name:query"
     click(SEARCH_BUTTON)
-    browser_lib.input_text(search_input, locator)
+    browser_lib.input_text(search_input, search_phrase)
     browser_lib.press_keys(search_input, "ENTER")
 
 
-def select_categories():
-    section_items = browser_lib.get_webelements("class:css-1qtb2wd")
+def select_categories(category_section):
+    section_items = browser_lib.get_webelements(category_section)
+    for item in section_items:
+        print(f"item: {item}")
+        print(f"txt: {item.text}")
+        for category in NEWS_CATEGORY:
+            print(f"{category}, {NEWS_CATEGORY}")
+            match = re.search(category, item.text, re.IGNORECASE)
+            if match:
+                item.click()
+    sleep(4)
+
+
+def iterate_news(news_selection):
+    breakpoint()
+    section_items = browser_lib.get_webelements(news_selection)
     for item in section_items:
         print(f"item: {item}")
         print(f"txt: {item.text}")
@@ -47,12 +63,14 @@ def close_browser():
 
 def main():
     """Start main function for RPA for news search"""
+    breakpoint()
     open_the_website(NYTIMES_URL)
     click(BREADCRUMB_BUTTON)
     type_search(SEARCH_PHRASE)
     click(CATEGORY_SELECTION)
-    select_categories()
-    sleep(10)
+    select_categories(CATEGORY_SECTION)
+    sleep(4)
+    iterate_news(NEWS_SELECTION)
 
 
 if __name__ == "__main__":
